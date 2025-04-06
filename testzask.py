8242d6af07c3985a93ff7be825c3e5c196f18c15
Luchetosh <jasenica2018@gmail.com>
2025-04-06 15:07:59 +0200

    first save

---
+++ testzask.py
@@ -0,0 +1,11 @@
+
+prvi_broj = float(input("Unesite prvi broj: "))
+drugi_broj = float(input("Unesite drugi broj: "))
+
+if prvi_broj == drugi_broj:
+    print('Brojevi su jednaki')
+
+if prvi_broj < drugi_broj:
+    print('Drugi broj je veci od prvog')
+else:
+    print('Prvi broj je veci od drugog')
