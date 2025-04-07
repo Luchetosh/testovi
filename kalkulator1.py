3691c43d231b1a1438cb6c97b7f5d12fa8d702b0
Luchetosh <jasenica2018@gmail.com>
2025-04-08 00:44:36 +0200

    zamjenjena sitnica u sintaxi kalkulator koda

--- kalkulator1.py
+++ kalkulator1.py
@@ -3,7 +3,7 @@ print("Kalkulator (+, -, /, *)")
 def kalkulator():
     try:
         broj1 = float(input("Unesi prvi broj: "))
-        print("Izaberij operaciju:\n 1 = +\n 2 = -\n 3 = /\n 4 = *")
+        print("Izaberij operaciju:\n 1 = +\n 2 = -\n 3 = *\n 4 = /")
         operacija = int(input("Operacija: "))
         broj2 = float(input("Unesi drugi broj: "))
     except ValueError:
@@ -15,12 +15,12 @@ def kalkulator():
     elif operacija == 2:
         rezultat = broj1 - broj2
     elif operacija == 3:
-        rezultat = broj1 / broj2
+        rezultat = broj1 * broj2
     elif operacija == 4:
         if broj2 == 0:
             print("ERROR, Djeljenje s nulom nije moguce. ")
             return None
-        rezultat = broj1 * broj2
+        rezultat = broj1 / broj2
     else:
         print("Nepoznata operacija. ")
         return None
